0eba46ea8b7a3708f7d70d33ed279cb3725c6c82
Intaniger <intaniger@gmail.com>
2023-06-13 13:18:35 +0700

    refactor: :truck: move page from /cleverse to homepage instead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,50 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Blog } from './api/blogs/route'
+import { Podcast } from './api/podcasts/route'
+import { BlogBrief, BlogWithImage } from './components/Blog'
+import Navbar from './components/Navbar'
+import { Podcast as PodcastComponent } from './components/Podcast'
 
-export default function Home() {
+const Index = async () => {
+  const [firstBlog, ...blogs] = await getBlogsData()
+  const podcasts = await getPodcastData()
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <Navbar />
+      <div className="flex flex-col justify-center pt-8 w-full h-max gap-y-8 max-w-4xl mx-auto">
+        <div className="px-4">
+          <BlogWithImage image={firstBlog.image!} {...firstBlog} />
         </div>
+        <section className="flex flex-col w-full gap-3 ">
+          <div className="w-full flex items-center font-semibold font-montserrat justify-between text-sectionHeader">
+            <h2 className="text-lg">Blog</h2>
+          </div>
+          <div className="grid grid-cols-2 gap-x-5 gap-y-8">
+            {blogs.map((blog) => (
+              <BlogBrief key={blog.href} {...blog} />
+            ))}
+          </div>
+        </section>
+
+        <section className="flex flex-col w-full gap-3 mt-4">
+          <div className="w-full flex items-center font-semibold font-montserrat justify-between text-sectionHeader">
+            <h2 className="text-lg">Podcast & Video</h2>
+          </div>
+          {/* <div className="grid grid-cols-2 gap-x-5 gap-y-8"> */}
+          {podcasts.map((p) => (
+            <PodcastComponent key={p.href} {...p} />
+          ))}
+          {/* </div> */}
+        </section>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   )
 }
+
+export const getBlogsData = (): Promise<Blog[]> =>
+  fetch('http://localhost:3000/api/blogs', {
+    next: { revalidate: 60 },
+  }).then((r) => r.json())
+
+const getPodcastData = (): Promise<Podcast[]> =>
+  fetch('http://localhost:3000/api/podcasts').then((r) => r.json())
+export default Index
